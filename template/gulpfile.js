df128bbcefa64fbd79c2ce40d139b6bcaa436635
Jyo Reddy <jjreddy@gmail.com>
2020-02-14 09:56:31 -0500

    updated

--- template/gulpfile.js
+++ template/gulpfile.js
@@ -1,6 +1,37 @@
 'use strict'
 var gulp = require('gulp');
 var requireDir = require('require-dir');
+// var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
 requireDir('gulp-tasks');
 
-gulp.task('default', gulp.series('serve'));
+
+
+gulp.task('mdc-default', function () {
+    return gulp.src('./assets/scss/**/style.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({outputStyle: 'expanded', includePaths: './node_modules'}).on('error', sass.logError))
+        .pipe(sourcemaps.write('./maps'))
+        .pipe(gulp.dest('./assets/css'));
+  });
+  gulp.task('components', function () {
+    return gulp.src('./assets/scss/**/components.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({outputStyle: 'expanded', includePaths: './node_modules'}).on('error', sass.logError))
+        .pipe(sourcemaps.write('./maps'))
+        .pipe(gulp.dest('./assets/css'));
+  });
+
+  gulp.task('mdc-layout', function () {
+    return gulp.src('./assets/scss/**/mdc-layout.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({outputStyle: 'expanded', includePaths: './node_modules'}).on('error', sass.logError))
+        .pipe(sourcemaps.write('./maps'))
+        .pipe(gulp.dest('./assets/css'));
+  });
+
+  gulp.task('default', gulp.series('mdc-default', 'components','mdc-layout'));
+  
+  // gulp.task('default', gulp.series('mdc-layout'));
+// gulp.task('default', gulp.series('serve'));
